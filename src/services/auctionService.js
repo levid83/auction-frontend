71adfc1077c40b56140c289436a3afd5feb0b83f
Levente Danel <danellevente@yahoo.com>
2020-10-09 21:08:53 +0300

    move  error handling to the calling functions

--- src/services/auctionService.js
+++ src/services/auctionService.js
@@ -1,31 +1,18 @@
 import HttpService from "./httpService";
 export default class AuctionService extends HttpService {
   async fetchAuctions() {
-    try {
-      const result = await this.get("auctions?status=OPEN");
-      return result.data;
-    } catch (err) {
-      console.error(err);
-    }
+    return this.get("auctions?status=OPEN");
   }
 
   async createAuction(title, pictureBase64) {
-    try {
-      const createAuctionResult = await this.post("auction", { title });
-      await this.patch(
-        `auction/${createAuctionResult.data.id}/picture`,
-        pictureBase64
-      );
-    } catch (err) {
-      console.error(err);
-    }
+    const createAuctionResult = await this.post("auction", { title });
+    await this.patch(
+      `auction/${createAuctionResult.data.id}/picture`,
+      pictureBase64
+    );
   }
 
   async placeBid(id, amount) {
-    try {
-      await this.patch(`auction/${id}/bid`, { amount });
-    } catch (err) {
-      console.error(err);
-    }
+    return this.patch(`auction/${id}/bid`, { amount });
   }
 }
